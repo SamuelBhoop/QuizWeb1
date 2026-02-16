8785709f34b29a00292238b34efba89f9392b7b3
SamuelBhoop <sabuca6@gmail.com>
2026-02-16 17:47:50 -0500

    commit de prueba

---
+++ Quiz1Web/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Quiz1Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "API funcionando correctamente";
+        }
+    }
+}
